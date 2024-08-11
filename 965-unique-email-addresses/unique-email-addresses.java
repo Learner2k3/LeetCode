803c338cf7e985b3ebeb2c6a97a0b4ca04fe7d11
Nivasika A <104685689+Learner2k3@users.noreply.github.com>
2024-08-11 12:12:45 +0530

    Time: 6 ms (97.64%) | Memory: 44.2 MB (85.55%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.java
+++ 965-unique-email-addresses/unique-email-addresses.java
@@ -1,18 +1,36 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int numUniqueEmails(String[] emails) {
-        Set<String> set = new HashSet<>(); 
-        for(int i =0; i<emails.length; i++){                     //n
-            String[] parts = emails[i].split("@");
-            String localName = parts[0];//test+email
-            String domainname = parts[1];//leetcode.com
-            String[] plusRemoved =  localName.split("\\+"); //te.st email
-            String concat = dotRemoved(plusRemoved[0]); 
-            String ouput = concat+"@" + domainname;
-            set.add(ouput);
+        Set<String> uniqueEmails = new HashSet<>();
+
+        for (String email : emails) {
+            // Find the position of '@' in the email
+            int atIndex = email.indexOf('@');
+            
+            // Extract the local part
+            String localPart = email.substring(0, atIndex);
+            
+            // Find the position of '+' in the local part, if it exists
+            int plusIndex = localPart.indexOf('+');
+            
+            // If '+' exists, ignore everything after it
+            if (plusIndex != -1) {
+                localPart = localPart.substring(0, plusIndex);
+            }
+            
+            // Remove all '.' characters from the local part
+            localPart = localPart.replace(".", "");
+            
+            // Concatenate the processed local part with the domain part
+            String normalizedEmail = localPart + email.substring(atIndex);
+            
+            // Add the normalized email to the set
+            uniqueEmails.add(normalizedEmail);
         }
-        return set.size(); 
-    } 
-    public static String dotRemoved(String str){//te.st
-            return str.replace(".","");
-   }
-} 
+
+        // Return the size of the set, which represents the number of unique emails
+        return uniqueEmails.size();
+    }
+}
