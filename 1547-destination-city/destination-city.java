1e34454935b24e7c4f699a3851ce24cfb3b0645c
Nivasika A <104685689+Learner2k3@users.noreply.github.com>
2024-08-13 06:15:18 +0530

    Time: 2 ms (99.83%) | Memory: 43.4 MB (33.52%) - LeetSync

---
+++ 1547-destination-city/destination-city.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        Set<String> set = new HashSet<>();
+        for(List<String> path : paths){
+            set.add(path.get(0));
+        }
+         for(List<String> path : paths){
+             String des = path.get(1);
+             if(!set.contains(des)){
+                 return des;
+             }
+         }
+        return "";
+    }
+}
