99cc465e430f9cf29889aef4920a08d43e458995
Nivasika A <nivasika24@gmail.com>
2024-08-19 06:00:00 +0530

    Time: 13 ms (75.84%) | Memory: 49.3 MB (8.32%) - LeetSync

---
+++ 554-brick-wall/brick-wall.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public int leastBricks(List<List<Integer>> wall) {
+          
+         Map<Integer, Integer> map = new HashMap<>();
+        for (List<Integer> row : wall) {
+            int sum = 0;
+            for (int i = 0; i < row.size() - 1; i++) {
+                sum += row.get(i);
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+        int max = 0;
+        for (int count : map.values()) {
+            max = Math.max(max, count);
+        }
+        return wall.size() - max;
+    }
+}
